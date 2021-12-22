ab9d7d45963e7cc4510d0b2b7a473325427c29a7
Paarthbathla338 <Paarth.bathla@outlook.com>
2021-12-22 22:21:55 +0530

    Google Fonts Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 // ignore_for_file: prefer_const_constructors
 
 import "package:flutter/material.dart";
+import 'package:google_fonts/google_fonts.dart';
 
 import 'screens/homepage.dart';
 import 'screens/login.dart';
+import "package:google_fonts/google_fonts.dart";
 
 void main() {
   runApp(MyApp());
@@ -14,7 +16,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       themeMode: ThemeMode.light,
-      darkTheme: ThemeData(primarySwatch: Colors.cyan),
+      darkTheme: ThemeData(
+          primarySwatch: Colors.cyan,
+          fontFamily: GoogleFonts.lato().fontFamily,
+          primaryTextTheme: GoogleFonts.latoTextTheme()),
       routes: {
         "/": (context) => LoginPage(),
         "/home": (context) => HomePage(),
